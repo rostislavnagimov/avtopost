299234291fcea8f696fdf5450aaa763e3aaa4e34
Rostislav Nagimov <rostislav.nagimov@gmail.com>
2023-10-16 14:03:17 -0700

    feat: cleanup and add ok status

--- server.py
+++ server.py
@@ -1,32 +1,26 @@
-from flask import Flask, request, Response
+from flask import Flask, request
 import requests
-import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello2'
+    return 'Server is running'
 
 @app.route('/post', methods=['POST'])
 def post():
-    messages = []
     if request.json.get('type'):
-        messages.append(request.json['type'])
         if request.json['type'] == 'wall_post_new':
-            messages.append('New post!')
-            res = requests.post(
+            requests.post(
                 f"https://api.telegram.org/bot6646276001:AAH4q6HZM2n_-m4mscDBgEuVhIBhYTiMJBw/sendMessage?chat_id=159529075&text={request.json['object']['text']}",
             )
-            messages.append(f'Response status: {res.status_code}')
+
+            return 'ok', 200
 
         if request.json['type'] == 'confirmation':
             return '92a043a8'
-    messages.append(json.dumps(request.json))
 
-    response = Response('\n'.join(messages))
-    response.headers['Content-Type'] = 'text/plain'
-    return response
+    return 'Invalid data'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
